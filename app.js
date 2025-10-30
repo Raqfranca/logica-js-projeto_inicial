51f1574b4a3cdfe7ec00e851b9e50eff037ff344
raqfranca <raqfranca@hotmail.com>
2025-10-29 22:36:25 -0300

    feet - finish first class

--- app.js
+++ app.js
@@ -3,6 +3,58 @@ alert("Bem vindo ao jogo do número secreto")
 
 let numeroSecreto = 29;
 
-prompt("Escolha um número entre 1 e 30");
+let chute =  prompt("Escolha um número entre 1 e 30");
+ 
 
+if (numeroSecreto == chute){
+    console.log('Isso ai, deu certo!!!')
+}
 
+alert("Boas vindas ao nosso site")
+
+let nome = "Lua;"
+
+let idade = 25;
+
+let numeroDeVendas = 50;
+
+let saldoDisponivel = 1000;
+
+alert ("Erro! Preencha todos os campos");
+
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+alert (mensagemDeErro)
+
+let nomeUser = prompt("Qual o seu nome?")
+
+let idadeUser = prompt("Qual a sua idade?")
+
+let idadeParaDigirir = 18
+
+if (idadeUser >= idadeParaDigirir){
+    alert("Já pode tirar habilitação!")
+}
+
+/* Desafios
+
+    Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+
+    Declare uma variável chamada nome e atribua a ela o valor "Lua".
+
+    Crie uma variável chamada idade e atribua a ela o valor 25.
+
+    Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+
+    Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+
+    Exiba um alerta com o texto "Erro! Preencha todos os campos"
+
+    Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+
+    Para o próximo código, use um novo prompt para perguntar o nome do usuário e armazená-lo em uma variável, pode chamá-la de nome ou adicionar o que desejar .
+
+    Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+
+    Agora, para validar a idade que capturamos no desafio 09, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+ */
